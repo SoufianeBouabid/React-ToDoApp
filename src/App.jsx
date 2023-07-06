756d4c0fb11333c35ccb092217cae2950c2812ca
BOUABID <BOUABID.Soufiane@bcg.com>
2023-07-06 17:21:51 +0100

    incorporate reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,149 @@
-import React, { useState } from "react";
-import { createContext } from "react";
-import FetchData from "./fetchData";
-import InputZone from "./inputZone";
+import React, { useState, useEffect, useReducer } from "react";
 
-export const MyContext = createContext({ posts: [], setPost: () => {} });//useReducer
+const ACTIONS = {
+  ADD_POST: "add-post",
+  DELETE_POST: "delete-post",
+  UPDATE_POST: "update-post",
+};
+
+function reducer(posts, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_POST:
+      return [action.payload, ...posts];
+
+    case ACTIONS.DELETE_POST:
+      return posts.filter((post) => post.id !== action.payload.id);
+
+    case ACTIONS.UPDATE_POST:
+      // Update the corresponding post in the array
+      return posts.map((post) =>
+        post.id === action.payload.id ? action.payload : post
+      );
+
+    default:
+      return posts;
+  }
+}
 
 function App() {
   const currentDate = new Date().toLocaleDateString();
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useReducer(reducer, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const userId = e.target.userId.value;
+    const id = Date.now();
+
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          id,
+          title,
+          userId,
+        }),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      const newPost = await res.json();
+
+      setPosts({ type: ACTIONS.ADD_POST, payload: newPost });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: "DELETE",
+      });
+
+      setPosts({ type: ACTIONS.DELETE_POST, payload: newPost });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const handleUpdate = async (id) => {
+    try {
+      await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          title: "Updated Title",
+          userId: 1,
+        }),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      const updatedPost = {
+        id,
+        title: "Updated Title",
+        userId: 1,
+      };
+
+      setPosts({ type: ACTIONS.UPDATE_POST, payload: updatedPost });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        const jsonData = await response.json();
+        console.log(jsonData);
+
+        setPosts({ type: ACTIONS.ADD_POST, payload: jsonData });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <>
       <div>
-        <MyContext.Provider value={{ posts, setPosts }}>
-          <InputZone />
-          <FetchData />
-        </MyContext.Provider>
+        <h2>Add new To Do</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="userId">UserId</label>
+          <input
+            type="text"
+            placeholder="insert your UserID title"
+            name="userId"
+          />
+          <label htmlFor="title">Title</label>
+          <textarea
+            type="text"
+            placeholder="insert a new Todo title"
+            name="title"
+          ></textarea>
+          <button type="submit">
+            <span>Add</span>
+          </button>
+        </form>
       </div>
-      <h1>To do list {currentDate}</h1>
-      <InputZone />
-      <FetchData />
+      <div>
+        {posts.map((post, index) => {
+          return (
+            <div key={post.id + index}>
+              <p>{post.userId}</p>
+              <h2>{post.title}</h2>
+              <button onClick={() => handleDelete(post.id)}>Delete</button>
+              <button onClick={() => handleUpdate(post.id)}>Update</button>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
--- src/inputZone.jsx
+++ src/inputZone.jsx
@@ -1,60 +1,60 @@
-import React, { useState, useEffect, useContext } from "react";
-import { MyContext } from "./App";
+// import React, { useState, useEffect, useContext } from "react";
+// import { MyContext } from "./App";
 
-function InputZone() {
-  const { posts, setPosts } = useContext(MyContext);
+// function InputZone() {
+//   const { posts, setPosts } = useContext(MyContext);
 
-  const handleSubmit = (e) => {
-    e.preventDefault(); //to prevent refreshing
-    console.log(e);
+//   const handleSubmit = (e) => {
+//     e.preventDefault(); //to prevent refreshing
+//     console.log(e);
 
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify({
-        title: e.target.title.value,
-        userId: e.target.userId.value,
-      }),
-      headers: {
-        "Content-type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((post) => {
-        setPosts((posts) => [post, ...posts]);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
+//     fetch("https://jsonplaceholder.typicode.com/posts", {
+//       method: "POST",
+//       body: JSON.stringify({
+//         title: e.target.title.value,
+//         userId: e.target.userId.value,
+//       }),
+//       headers: {
+//         "Content-type": "application/json",
+//       },
+//     })
+//       .then((res) => res.json())
+//       .then((post) => {
+//         setPosts((posts) => [post, ...posts]);
+//       })
+//       .catch((err) => {
+//         console.log(err.message);
+//       });
+//   };
 
-  return (
-    <div>
-      <h2>Add new To Do</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="userId">UserId</label>
-        <input
-          type="text"
-          placeholder="insert your UserID title"
-          id="userId"
-          name="userId"
-        />
-        <label htmlFor="title">Title</label>
-        <textarea
-          placeholder="insert a new Todo title"
-          id="title"
-          name="title"
-          //Each input element should have a unique name attribute so that the data entered in the input can be identified and accessed on the server-side when the form is submitted
-        ></textarea>
-        <button type="submit">
-          <span>Add</span>
-        </button>
-      </form>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h2>Add new To Do</h2>
+//       <form onSubmit={handleSubmit}>
+//         <label htmlFor="userId">UserId</label>
+//         <input
+//           type="text"
+//           placeholder="insert your UserID title"
+//           id="userId"
+//           name="userId"
+//         />
+//         <label htmlFor="title">Title</label>
+//         <textarea
+//           placeholder="insert a new Todo title"
+//           id="title"
+//           name="title"
+//           //Each input element should have a unique name attribute so that the data entered in the input can be identified and accessed on the server-side when the form is submitted
+//         ></textarea>
+//         <button type="submit">
+//           <span>Add</span>
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
 
-export default InputZone; //nom de fichier et component majucule
-//validation fu form avant de pusher via react hook form pour validation de formulaire
-//userId number title required
-//tout géré par contexte api
-//delete and patch call api
+// export default InputZone; //nom de fichier et component majucule
+// //validation fu form avant de pusher via react hook form pour validation de formulaire
+// //userId number title required
+// //tout géré par contexte api
+// //delete and patch call api
