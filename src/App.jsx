b80ef37bda88702d900441a6a0118e83e52635f5
BOUABID <BOUABID.Soufiane@bcg.com>
2023-07-07 15:05:25 +0100

    debug reducer function

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ function reducer(posts, action) {
       return posts.filter((post) => post.id !== action.payload.id);
 
     case ACTIONS.UPDATE_POST:
-      // Update the corresponding post in the array
       return posts.map((post) =>
         post.id === action.payload.id ? action.payload : post
       );
@@ -26,22 +25,19 @@ function reducer(posts, action) {
 }
 
 function App() {
-  const currentDate = new Date().toLocaleDateString();
   const [posts, setPosts] = useReducer(reducer, []);
+  const [userId, setUserId] = useState("");
+  const [title, setTitle] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const title = e.target.title.value;
-    const userId = e.target.userId.value;
-    const id = Date.now();
 
     try {
       const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
         method: "POST",
         body: JSON.stringify({
-          id,
-          title,
-          userId,
+          userId: userId,
+          title: title,
         }),
         headers: {
           "Content-type": "application/json",
@@ -62,7 +58,7 @@ function App() {
         method: "DELETE",
       });
 
-      setPosts({ type: ACTIONS.DELETE_POST, payload: newPost });
+      setPosts({ type: ACTIONS.DELETE_POST, payload: { id } });
     } catch (err) {
       console.log(err.message);
     }
@@ -97,11 +93,9 @@ function App() {
     async function fetchData() {
       try {
         const response = await fetch(
-          "https://jsonplaceholder.typicode.com/todos"
+          "https://jsonplaceholder.typicode.com/posts"
         );
         const jsonData = await response.json();
-        console.log(jsonData);
-
         setPosts({ type: ACTIONS.ADD_POST, payload: jsonData });
       } catch (error) {
         console.log(error);
@@ -113,37 +107,36 @@ function App() {
   return (
     <>
       <div>
-        <h2>Add new To Do</h2>
+        <h2>Add new Post</h2>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="userId">UserId</label>
+          <label htmlFor="userId">User ID</label>
           <input
             type="text"
-            placeholder="insert your UserID title"
-            name="userId"
+            placeholder="Enter User ID"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
           />
           <label htmlFor="title">Title</label>
           <textarea
-            type="text"
-            placeholder="insert a new Todo title"
-            name="title"
+            placeholder="Enter Post Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           ></textarea>
           <button type="submit">
             <span>Add</span>
           </button>
         </form>
       </div>
-      <div>
-        {posts.map((post, index) => {
-          return (
-            <div key={post.id + index}>
-              <p>{post.userId}</p>
-              <h2>{post.title}</h2>
-              <button onClick={() => handleDelete(post.id)}>Delete</button>
-              <button onClick={() => handleUpdate(post.id)}>Update</button>
-            </div>
-          );
-        })}
-      </div>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <p>{post.userId}</p>
+            <h2>{post.title}</h2>
+            <button onClick={() => handleDelete(post.id)}>Delete</button>
+            <button onClick={() => handleUpdate(post.id)}>Update</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
